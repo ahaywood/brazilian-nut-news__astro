6bed3d523cfbb26dd6871c2ec78597026c4762f2
Amy Haywood Dutton <me@amyhaywood.com>
2024-04-30 15:11:52 -0700

    ✨ Added Vercel config for Deploying

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import db from "@astrojs/db";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  }), mdx(), db(), react()]
+  }), mdx(), db(), react()],
+  output: "server",
+  adapter: vercel()
 });
